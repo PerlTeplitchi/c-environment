c49c88de0a54310a3ce44e277c58a75e1560e44e
El Che <mine.paillou@gmail.com>
2020-12-24 15:29:36 +0100

    programme presque complet

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+/*
+programme de PERL
+
 #include <stdio.h>
 int main()
 {
@@ -5,4 +8,39 @@ int main()
 		printf("%d\n",i);
 	}
 		return 0;
+}
+*/
+
+// prgm jm, seulement le ./enumerate_number *avec rien* ne fonctionne pas
+
+
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char **argv[])
+{
+	int n = atoi(argv[1]);
+	
+	if(n>0 && n<11){
+		for (int i = 1; i <= n; ++i)
+		{
+			printf("%d\n", i);
+		}
+	}
+	if (n==0)
+	{
+		return 0;
+	}
+	for (int i = 0; argv[i]!='\0'; ++i)
+	{
+		if (argv[i]>= 'A' && argv[i]<= 'Z')
+		{
+			return 0;
+		}
+		else if (argv[i] >= 'a' && argv[i]<= 'z')
+		{
+			return 0;
+		}
+	}
+
 }
