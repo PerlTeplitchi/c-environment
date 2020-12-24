b1a770823323dcc8d2694d3324d2e52ad783da3a
El Che <mine.paillou@gmail.com>
2020-12-24 15:52:07 +0100

    CODE FONCTIONEL !

--- main.c
+++ main.c
@@ -19,6 +19,8 @@ int main()
 #include <string.h>
 int main(int argc, char **argv[])
 {
+	if (argc>1)
+	{
 	int n = atoi(argv[1]);
 	
 	if(n>0 && n<11){
@@ -40,6 +42,15 @@ int main(int argc, char **argv[])
 		else if (argv[i] >= 'a' && argv[i]<= 'z')
 		{
 			return 0;
+		}
+	}
+	}
+	
+	else if(argc == 1)
+	{
+		for (int i = 1; i <=10; ++i)
+		{
+			printf("%d\n", i);
 		}
 	}
 
